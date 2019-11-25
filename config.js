de8136927bd1b00e7ef6359917e34a8e3e89f537
VKapicyn <goodsnasey@gmail.com>
2019-11-25 16:03:58 +0300

    вместо stash

--- config.js
+++ config.js
@@ -1,12 +1,20 @@
 exports.port = 8888;
-exports.token = 'super_sicret_parol';
+exports.token = 'super_sicret_parol_RANHIGS';
 exports.dbName = 'sessions';
 exports.adminLogins = ['Bank'];
 exports.userDB = 'game_users';
 exports.opsDB = 'game_ops';
-exports.projectGoups = ['01','02','03','04','05','06'];
-exports.tables = ['X', 'Y', 'Z', 'A', 'B', 'C','D'];
-exports.newProjects = ['Project_1', 'Project_2', 'Project_3', 'Project_4', 'Project_5', 'Project_6', 'Project_7', 'Project_8', 'Project_9', 'Project_10']
+exports.projectGoups = ['01','02','03','04','05','06', '07', '08', '09'];
+exports.tables = ['X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'H'];
+exports.newProjects = [
+    'Project_1', 'Project_2', 'Project_3', 
+    'Project_4', 'Project_5', 'Project_6', 
+    'Project_7', 'Project_8', 'Project_9', 
+    'Project_10', 'Project_11', 'Project_12',
+    'Project_13', 'Project_14', 'Project_15',
+    'Project_16', 'Project_17', 'Project_18',
+    'Project_19', 'Project_20', 'Project_21'
+]
 exports.licExp = ['Оплата труда', 'Оплата проектных услуг', 'Возврат инвестиции'];
 exports.lic = [];
 exports.creditLimit = 300;
