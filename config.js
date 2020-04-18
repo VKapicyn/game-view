b9a046bc1730bd8e6ba63f6d29a46395b89e51ea
IvanProtsenko <ivnprotsenko@gmail.com>
2020-04-18 13:10:34 +0300

    updated

--- config.js
+++ config.js
@@ -6,5 +6,5 @@ exports.userDB = 'game_users';
 exports.opsDB = 'game_ops';
 exports.projectGoups = ['01','02','03','04','05','06', '07', '08', '09'];
 exports.tables = ['A', 'B', 'C', 'D', 'E', 'F', 'H', 'K', 'L', 'М','N','O','P','R','S','T','U','V','W','X','Y','Z'];
-exports.sentEmail = "ivnprotsenko@gmail.com";
-exports.sentPass = "Abc581321";
+exports.sentEmail = "";
+exports.sentPass = "";
